17e9f954a97d1eb14812de652b8110b19419027b
Cesare Ferrari <cesare@maryandferrari.com>
2019-04-13 13:06:15 -0400

    Show info box when over logo

--- js/index.js
+++ js/index.js
@@ -1,20 +1,44 @@
 // Your code goes here
 
+// Alert message on page load
 const alertMessage = document.createElement('div');
 alertMessage.classList.add('alert');
 alertMessage.textContent = "Welcome to our web site. I hope you enjoy it!";
 
+// Close Button for alert message
 const closeAlert = document.createElement('span');
 closeAlert.textContent = 'X';
 closeAlert.classList.add('close');
 alertMessage.append(closeAlert);
 
+closeAlert.addEventListener('click', (event) => {
+  alertMessage.classList.add('hidden');
+})
 
 window.addEventListener('load', (event) => {
   console.log('Page is loaded');
   document.body.prepend(alertMessage);
 })
 
-closeAlert.addEventListener('click', (event) => {
-  alertMessage.classList.add('hidden');
+
+// Info box
+
+const logo = document.querySelector('.logo-heading');
+logo.style.position = 'relative';
+
+const infoBox = document.createElement('div');
+infoBox.classList.add('info-box');
+logo.append(infoBox);
+
+
+logo.addEventListener('mouseover', (event) => {
+  infoBox.textContent = event.target.dataset.info;
+  infoBox.classList.add('visible');
 })
+
+logo.addEventListener('mouseout', (event) => {
+  infoBox.textContent = event.target.dataset.info;
+  infoBox.classList.remove('visible');
+})
+
+
