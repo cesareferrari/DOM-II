d9f10ec707f9fb0dbb9ffb5a4a9d270cfc4f1444
Cesare Ferrari <cesare@maryandferrari.com>
2019-04-13 12:35:51 -0400

    Add alert message on page load

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+const alertMessage = document.createElement('div');
+alertMessage.classList.add('alert');
+alertMessage.textContent = "Welcome to our web site. I hope you enjoy it!";
+
+const closeAlert = document.createElement('span');
+closeAlert.textContent = 'X';
+closeAlert.classList.add('close');
+alertMessage.append(closeAlert);
+
+
+window.addEventListener('load', (event) => {
+  console.log('Page is loaded');
+  document.body.prepend(alertMessage);
+})
+
+closeAlert.addEventListener('click', (event) => {
+  alertMessage.classList.add('hidden');
+})
